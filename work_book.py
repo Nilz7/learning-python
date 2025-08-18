e20dae993f71f3f9ebbb741d680dce20655fff68
Niloofar Sharafadinzadeh <niloofarsharafadinzadeh@Niloofars-MacBook-Air.local>
2025-08-17 18:33:38 -0600

    updated work book

--- work_book.py
+++ work_book.py
@@ -1,15 +1,31 @@
-def calculator(num1, num2, operation):
-	match operation:
-		case 'plus':
-			print(f"{num1 + num2}");
-		case 'minus':
-			print(f"{num1 - num2}");
-		case 'divide':
-			print(f"{num1 / num2}");
-		case 'multiply':
-			print(f"{num1 * num2}");
+# names = ["Screws", 'Wheels', 'Metal parts', 'Rubber bits', 'Screwdrivers', 'Wood']
+# numbers = [43, 12, 95, 421, 23, 43]
+# replenish_list = [(name, number) for name,number in zip(names, numbers) if number < 25]
+# print(replenish_list)
+# combined_list = [[f"{letter}{num}" for num in range(1,9)] for letter in 'ABCDEfGH']
+# for row in combined_list:
+#     print(row)
 
-calculator(10,4,'plus')
-calculator(10,4,'minus')
-calculator(10,4,'divide')
-calculator(10,4,'multiply')
+# set = {num for num in range(4)}
+# print(set)÷
+# tuple = tuple(num for num in range(4))
+# print(tuple)
+
+# combined_list = list(zip(names, numbers))
+# # print(combined_list)
+# print(sorted(combined_list, key = lambda item: item[1]))
+# print(sorted(combined_list, key = lambda item: len(item[0])))
+
+# power_list = [num ** 2 for num in range(1,6)]
+# filter_list = [num for num in range(1,6) if num < 4]
+# print(power_list)
+# print(filter_list)
+
+with open('tree.txt', 'w') as file:
+    file.write('\t  *\n')
+    file.write('\t ***\n')
+    file.write('\t*****\n')
+    file.write('\t  *\n')
+    file.write('\t  *\n')
+with open('tree.txt', 'r') as file:
+    print(file.read())
