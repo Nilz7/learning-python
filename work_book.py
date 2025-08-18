3e0d1c3eb431b88d55f44d91e618baa7e27ebd82
Niloofar Sharafadinzadeh <niloofarsharafadinzadeh@Niloofars-MacBook-Air.local>
2025-08-17 19:24:02 -0600

    python hacker rank practice

--- work_book.py
+++ work_book.py
@@ -21,11 +21,35 @@
 # print(power_list)
 # print(filter_list)
 
-with open('tree.txt', 'w') as file:
-    file.write('\t  *\n')
-    file.write('\t ***\n')
-    file.write('\t*****\n')
-    file.write('\t  *\n')
-    file.write('\t  *\n')
-with open('tree.txt', 'r') as file:
-    print(file.read())
+# with open('tree.txt', 'w') as file:
+#     file.write('\t  *\n')
+#     file.write('\t ***\n')
+#     file.write('\t*****\n')
+#     file.write('\t  *\n')
+#     file.write('\t  *\n')
+# with open('tree.txt', 'r') as file:
+#     print(file.read())
+
+# x = 1
+# y = 1
+# z = 1
+# inputs = [x,y,z]
+# n = 2
+
+# list_num = [[i,j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k) != n]
+# print(list_num)
+
+n = 5
+arr = map(int, '2 3 6 6 5'.split())
+my_list = list(arr)
+my_list.sort(reverse=True)
+print(my_list)
+runner_score = 0
+high_score = my_list[0]
+for index,num in enumerate(my_list):
+    if num == high_score: continue
+    runner_score = num
+    break
+print(runner_score)
+
+    
